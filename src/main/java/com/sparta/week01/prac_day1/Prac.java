aeed154fa4cc39dee49e1749296f59cc6680a54f
koreait-shopping <icepri3535@gmail.com>
2022-09-06 14:39:15 +0900

    1day finish

--- src/main/java/com/sparta/week01/prac_day1/Prac.java
+++ src/main/java/com/sparta/week01/prac_day1/Prac.java
@@ -1,4 +1,4 @@
-package com.sparta.week01.prac;
+package com.sparta.week01.prac_day1;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/main/java/com/sparta/week01/prac_day2/Prac.java
@@ -0,0 +1,43 @@
+package com.sparta.week01.prac_day2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prac {
+
+    public static String celebs(String celeb) {
+        List<String> celebs = new ArrayList<>();
+        celebs.add("아이유");
+        celebs.add("린다G");
+        celebs.add("은비");
+        celebs.add("금비");
+        celebs.add("비");
+        celebs.add("차은우");
+        celebs.add("남주혁");
+        celebs.add("수지");
+        celebs.add("정우성");
+        celebs.add("제니");
+        celebs.add("정국");
+
+
+        String introduce = "";
+
+        for (int i = 0; i < celebs.size(); i++) {
+            String c = celebs.get(i);
+            if (c == celeb) {
+                System.out.println(c + "입니다.");
+            }
+        }
+        return introduce;
+    }
+
+        public static void main (String[]args){
+
+            String iu = celebs("아이유");
+            String suzy = celebs("수지");
+            System.out.println(iu);
+            System.out.println(suzy);
+
+
+        }
+    }
